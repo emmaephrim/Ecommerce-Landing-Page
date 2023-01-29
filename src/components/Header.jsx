c98085ce3975222de07d1140b2f8bc5ddaa2b4dd
emmanuel ephrim <emmaephrim@gmail.com>
2023-01-29 17:59:34 +0000

    feat(navbar): mobile implementation for navbar {search icon not functioning}

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import shop_Logo from "../images/Plenuu_logo_main.png";
 
 function Header() {
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
   return (
     <React.Fragment>
-      <header>
+      {/* <header>
         <div className="sub-head-one">
           <img src={shop_Logo} alt="" />
           <ul>
@@ -32,124 +40,51 @@ function Header() {
             </li>
           </ul>
         </div>
-      </header>
+      </header> */}
 
-      {/* <div className="d-flex justify-content-between">
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-          <a className="navbar-brand" href="#">
-            <img src={shop_Logo} alt="" style={{ width: "100px" }} />
+      <div className="nav">
+        <div className="topnav" id="myTopnav">
+          <a
+            href="javascript:void(0);"
+            className="icon"
+            onClick={myFunction}
+            style={{ color: "black", fontSize: "30px" }}
+          >
+            <i className="fa fa-bars bi bi-list"></i>
           </a>
-          <div className="container-fluid">
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarTogglerDemo01"
-              aria-controls="navbarTogglerDemo01"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    Men
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    Women
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    Vendor
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-        <div className="d-flex">
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
+          <a href="logo" className="logo  active">
+            <img
+              src={shop_Logo}
+              alt=""
+              // style={{ width: "80px", height: "40px" }}
             />
-            <button className="btn btn-outline-success" type="submit">
-              Search
+          </a>
+          <a href="#men">Men</a>
+          <a href="#women">Women</a>
+          <a href="#vendor">Vendor</a>
+        </div>
+        <div className="sub-head-two">
+          <span className="search_icon">
+            <i className="bi bi-search"></i>
+          </span>
+          <form>
+            <input type="search" placeholder="Search..." name="search" />
+            <button type="submit" className="bg-transparent  border-0 pe-3">
+              <i className="bi bi-search"></i>
             </button>
           </form>
-          <ul className="d-flex">
-            <li style={{}}>
+          <ul>
+            <li>
+              {/* < style={{ marginLeft: "-10px" }}> */}
               <i className="bi bi-cart2"></i>
             </li>
-            <li style={{}}>
+            <li>
+              {/* < style={{ marginLeft: "20px", marginRight: "5px" }}> */}
               <i className="bi bi-person"></i>
             </li>
           </ul>
         </div>
-      </div> */}
-      {/* <div>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-          <div className="container-fluid">
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <a className="navbar-brand " href="#">
-              Navbar
-            </a>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Features
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Pricing
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link disabled">Disabled</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </div> */}
-      {/* <nav>
-        <div>
-          <img src={shop_Logo} alt="" style={{ width: "6%" }} />
-        </div>
-        <div>
-          <ul>
-            <li>Home</li>
-            <li>Blog</li>
-            <li>Projects</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </div>
-      </nav> */}
+      </div>
     </React.Fragment>
   );
 }
