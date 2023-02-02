ba2973ca1d71b6627b911d3e72d14dd110792626
emmanuel ephrim <emmaephrim@gmail.com>
2023-02-02 09:25:23 +0000

    fix(special offer): title and postion fixed but side card width not fixed

--- src/components/SpecialOffers.jsx
+++ src/components/SpecialOffers.jsx
@@ -4,22 +4,13 @@ import special_item from "../images/special_item.png";
 function SpecialOffers() {
   return (
     <div className="side_card">
-      <div
+      <h1
         style={{
-          backgroundColor: "#d0e7fe",
-          paddingTop: "10px",
-          paddingLeft: "5px",
-          paddingBottom: "10px",
-          borderRadius: "15px",
-          fontWeight: "bolder",
-          fontSize: "140%",
-          width: "180px",
-          // textAlign: "center",
-          // alignItems: "start",
+          textAlign: "start",
         }}
       >
-        SPECIAL OFFERS
-      </div>
+        <strong className="special_offer_title">SPECIAL OFFERS</strong>
+      </h1>
       <span>BLACK GREY PASUA</span>
       <span>DRESS</span>
       <span style={{ color: "#4086d9" }}>GHC 250</span>
